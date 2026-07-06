de17f4958c6fdde3edf45d6b72c9a49c441693d4
Andrew <berladandreiii@gmail.com>
2026-07-06 19:36:56 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,9 +4,13 @@ class Settings():
     def __init__(self):
         """Инициализирует настройки игры."""
         self.screen_width = 1200
-        self.screen_height = 840
+        self.screen_height = 800
         self.bullet_speed_factor = 15
         self.bullet_width = 5
         self.bullet_height = 20
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
+        self.alien_speed_factor = 4
+        self.fleet_drop_speed = 45
+        self.fleet_direction = 1
+        self.ship_limit = 3
